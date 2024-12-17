185a7fd444530e1e04299a4425c9f8f77988761a
zanadukhan <cchiu231998@hotmail.ca>
2024-12-16 20:35:13 -0800

    implement password entry creation with form inputs and controller integration

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 
 import express from "express";
 import expressEjsLayouts from "express-ejs-layouts";
+import { passController } from "./controller/pass_controller.js";
 
 const app = express();
 app.set('view engine', 'ejs');
+
 app.use(expressEjsLayouts)
+app.use(express.urlencoded({ extended: true }));
 
 const port = 3000;
 
@@ -17,9 +20,8 @@ app.get('/create', (req, res) => {
     res.render('create');
 })
 
-app.post('/createpass', (req, res) => {
-    pass
-})
+app.post('/createpass', passController.createEntry)
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
     });
--- controller/pass_controller.js
+++ controller/pass_controller.js
@@ -1,4 +1,7 @@
-import PasswordGenerator from "./utils/password-generator.js";
+import PasswordGenerator from "../utils/password-generator.js";
+import { PrismaClient, Prisma } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const pass = new PasswordGenerator(10);
 
@@ -6,5 +9,16 @@ pass.generate();
 
 
 let passController = {
-
+    createEntry: async (req, res) => {
+        await prisma.loginItem.create({
+            data: {
+                name: req.body.entry_name,
+                service: req.body.service_name,
+                password: req.body.password
+            },
+        });
+        res.redirect('/');
+    }
 };
+
+export {passController}
