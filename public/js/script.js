c6cfdf038ea0c30f097e7bce7bb8e48c04a4547f
zanadukhan <cchiu231998@hotmail.ca>
2024-12-20 11:36:57 -0800

    feat: add dotenv dependency and enhance iframe functionality with close button

--- public/js/script.js
+++ public/js/script.js
@@ -11,23 +11,4 @@ window.generate = () => {
     
 }
 
-
-let view_buttons = document.getElementsByClassName("view");
-let iframe_div = document.getElementById("view-frame");
-const iframe = document.getElementById('iframe');
-
-console.log(view_buttons)
-
-for (let button of view_buttons) {
-    button.addEventListener('click', () => {
-        const entryId = button.getAttribute('data-id')
-        let visiblity_style = window.getComputedStyle(iframe_div)
-        let iframe_visiblity = visiblity_style.getPropertyValue('visibility')
-        if (iframe_visiblity === 'collapse') {
-            iframe_div.style.visibility = 'visible';
-        }
-        iframe.src = `/entry/${entryId}`;
-    })
-}
-
 export {generate}
